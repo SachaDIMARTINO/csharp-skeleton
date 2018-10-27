fb19eb17061bdbae6a2f633b4b75ec47cd54dba8
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-27 13:48:36 +0100

    q6 djikstra modif

--- Answers/Question6.cs
+++ Answers/Question6.cs
@@ -9,21 +9,20 @@ using System.Collections.Generic;
         public static int Answer(int numOfServers, int targetServer, int[,] connectionTimeMatrix)
         {
             //TODO: Please work out the solution;
-            if(numOfServers == 0 || numOfServers > connectionTimeMatrix.GetLength(0)) {
+            int nRow = connectionTimeMatrix.GetLength(0);
+            if(numOfServers == 0 || numOfServers > nRow) {
               return -1;
             }
-            int infinity = 1000000;
-            List<int> sommetList = new List<int>();
+            int infinity = 10000000;
             List<int> djikstra = new List<int>();
-            for(int i = 0; i < connectionTimeMatrix.GetLength(0); i++) {
-              sommetList.Add(i);
+            for(int i = 0; i < nRow; i++) {
               djikstra.Add(infinity);
             }
             // Lets visit the first node
             djikstra[0] = connectionTimeMatrix[0, 0];
             List<int> visited = new List<int>();
             visited.Add(0);
-            foreach(int node in sommetList) {
+            for(int node = 0; node < nRow; node++) {
               if (!visited.Contains(node)) {
                 djikstra[node] = Math.Min(djikstra[node], djikstra[0] + connectionTimeMatrix[0, node]);
               }
@@ -32,7 +31,7 @@ using System.Collections.Generic;
             while (visited.Count < numOfServers) {
               int nextNode = findNextNode(newDjikstra, visited, infinity); //TODO
               visited.Add(nextNode);
-              foreach(int node in sommetList) {
+              for(int node = 0; node < nRow; node++) {
                 if (!visited.Contains(node)) {
                   djikstra[node] = Math.Min(djikstra[node], djikstra[nextNode] + connectionTimeMatrix[nextNode, node]);
                 }
