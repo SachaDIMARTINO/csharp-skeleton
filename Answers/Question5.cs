2165d32f1386c3c32e7d11c4e14fef62e9357a7f
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-27 03:05:00 +0100

    q5

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 ﻿namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question5
@@ -5,7 +8,30 @@
         public static int Answer(int[] numOfShares, int totalValueOfShares)
         {
             //TODO: Please work out the solution;
-            return -1;
+            int answer = totalValueOfShares + 1;
+            int[] setArr = numOfShares.Distinct().ToArray();
+            Array.Sort(setArr);
+            Array.Reverse(setArr);
+            for (int i = 0; i < setArr.Length; i++) {
+              int j = i;
+              int remaining = totalValueOfShares;
+              int res = 0;
+              while (remaining > 0 && j < setArr.Length) {
+                int n = remaining / setArr[j];
+                res = res + n;
+                remaining = remaining - n * setArr[j];
+                j++;
+              }
+              if (remaining == 0) {
+                answer = Math.Min(answer, res);
+              }
+            }
+            if (answer == totalValueOfShares + 1) {
+              return 0;
+            }
+            else {
+              return answer;
+            }
         }
     }
 }
