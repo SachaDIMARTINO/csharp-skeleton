3019c27e88f5e3f1ad7b79f3b690c814ace7650d
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-27 02:03:37 +0100

    q4

--- Answers/Question4.cs
+++ Answers/Question4.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 ﻿namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question4
@@ -5,7 +8,37 @@
         public static int Answer(string[,] machineToBeFixed, int numOfConsecutiveMachines)
         {
             //TODO: Please work out the solution;
-            return -1;
+            string[][] newMachineToBeFixed = new string[machineToBeFixed.GetLength(0)][];
+            for(int i = 0; i < machineToBeFixed.GetLength(0); i++) {
+              string[] newLine = new string[machineToBeFixed.GetLength(1)];
+              for(int j = 0; j < machineToBeFixed.GetLength(1); j++) {
+                newLine[j] = machineToBeFixed[i,j];
+              }
+              newMachineToBeFixed[i] = newLine;
+            }
+            int answer = 1000000;
+            if (machineToBeFixed.GetLength(0) == 0 || numOfConsecutiveMachines <= 0 || numOfConsecutiveMachines > 100) {
+              return 0;
+            }
+            foreach(string[] row in newMachineToBeFixed) {
+              List<int> rowInt = new List<int>();
+              foreach(string elt in row) {
+                if(elt != "X") {
+                  rowInt.Add(int.Parse(elt));
+                }
+              }
+              for(int start = 0; start < rowInt.Count - numOfConsecutiveMachines + 1; start++) {
+                int end = start + numOfConsecutiveMachines;
+                List<int> shortRow = rowInt.GetRange(start, end);
+                answer = Math.Min(answer, shortRow.Sum());
+              }
+            }
+            if (answer == 1000000) {
+              return 0;
+            }
+            else {
+              return answer;
+            }
         }
     }
 }
