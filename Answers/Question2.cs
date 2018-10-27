350ca5085c79786f2e1ccf5983c15fe79f50dc4d
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-27 22:25:30 +0100

    q2

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -1,11 +1,67 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 ﻿namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
+        public static IList<IList<T>> PowerSet<T>(IList<T> list)
+        {
+          int n = 1 << list.Count;
+          IList<IList<T>> powerset = new List<IList<T>>();
+          for (int i = 0; i < n; ++i)
+          {
+            IList<T> set = new List<T>();
+            for (int bits = i, j = 0; bits != 0; bits >>= 1, ++j)
+            {
+              if ((bits & 1) != 0)
+                set.Add(list[j]);
+            }
+            powerset.Add(set);
+          }
+          return powerset;
+        }
         public static int Answer(int[] cashflowIn, int[] cashflowOut)
         {
             //TODO: Please work out the solution;
-            return 0;
+            IList<IList<int>> PScashflowIn = PowerSet(cashflowIn);
+            List<int> sumPSCashflowIn = new List<int>();
+            foreach(IList<int> set in PScashflowIn) {
+              int n = set.Sum();
+              if(n > 0) {
+                sumPSCashflowIn.Add(n);
+              }
+            }
+
+            IList<IList<int>> PScashflowOut = PowerSet(cashflowOut);
+            List<int> sumPSCashflowOut = new List<int>();
+            foreach(IList<int> set in PScashflowOut) {
+              int n = set.Sum();
+              if(n > 0) {
+                sumPSCashflowOut.Add(n);
+              }
+            }
+
+            int i = 0;
+            List<int> sumPSMin = new List<int>();
+            List<int> sumPSMax = new List<int>();
+            if (sumPSCashflowIn.Count > sumPSCashflowOut.Count) {
+              sumPSMin = sumPSCashflowOut;
+              sumPSMax = sumPSCashflowIn;
+            }
+            else {
+              sumPSMin = sumPSCashflowIn;
+              sumPSMax = sumPSCashflowOut;
+            }
+            while(true) {
+              foreach(int sum in sumPSMin) {
+                if(sumPSMax.Contains(sum + i) || sumPSMax.Contains(sum - i)) {
+                  int x = Math.Min(i, sumPSCashflowIn.Min());
+                  return Math.Min(x, sumPSCashflowOut.Min());
+                }
+                i++;
+              }
+            }
         }
     }
 }
