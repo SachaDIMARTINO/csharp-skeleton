60c53dd8fec661c422ea47e62ca221399aa63268
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-31 22:59:52 +0000

    q2 3 5

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -35,39 +35,3 @@ using System.Collections.Generic;
         }
     }
 }
-
-/*
-{
-    public class Question5
-    {
-        public static int Answer(int[] numOfShares, int totalValueOfShares)
-        {
-            //TODO: Please work out the solution;
-            int answer = totalValueOfShares + 1;
-            int[] setArr = numOfShares.Distinct().ToArray();
-            Array.Sort(setArr);
-            if (setArr.Contains(0)) {
-              setArr = setArr.Skip(1).ToArray();
-            }
-            Array.Reverse(setArr);
-            for (int i = 0; i < setArr.Length; i++) {
-              int j = i;
-              int remaining = totalValueOfShares;
-              int res = 0;
-              while (remaining > 0 && j < setArr.Length) {
-                int n = remaining / setArr[j];
-                res = res + n;
-                remaining = remaining - n * setArr[j];
-                j += 1;
-              }
-              if (remaining == 0) {
-                if (res < answer) {
-                  answer = res;
-                }
-              }
-            }
-            return (answer == totalValueOfShares + 1) ? 0 : answer;
-        }
-    }
-}
-*/
