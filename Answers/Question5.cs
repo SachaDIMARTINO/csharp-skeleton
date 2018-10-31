dcd5227e01e341039c2d878f997934643b7197ec
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-31 19:43:50 +0000

    q5

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -8,6 +8,41 @@ using System.Collections.Generic;
         public static int Answer(int[] numOfShares, int totalValueOfShares)
         {
             //TODO: Please work out the solution;
+            if (numOfShares.Length == 0) {
+              return 0;
+            }
+            int[] setArr = numOfShares.Distinct().ToArray();
+            Array.Sort(setArr);
+            if (setArr.Contains(0)) {
+              setArr = setArr.Skip(1).ToArray();
+            }
+            Array.Reverse(setArr);
+            int infinity = 1000000;
+            int[] resArr = new int[totalValueOfShares + 1];
+            resArr[0] = 0;
+            for (int i = 1; i <= totalValueOfShares; i++) {
+              resArr[i] = infinity;
+            }
+
+            for (int i = 0; i < setArr.Length; i++) {
+              for (int j = 1; j <= totalValueOfShares; j++) {
+                if(setArr[i] <= j && (resArr[j - setArr[i]] + 1) < resArr[j]) {
+                  resArr[j] = resArr[j - setArr[i]] + 1;
+                }
+              }
+            }
+            return (resArr[totalValueOfShares] != infinity) ? resArr[totalValueOfShares] : 0;
+        }
+    }
+}
+
+/*
+{
+    public class Question5
+    {
+        public static int Answer(int[] numOfShares, int totalValueOfShares)
+        {
+            //TODO: Please work out the solution;
             int answer = totalValueOfShares + 1;
             int[] setArr = numOfShares.Distinct().ToArray();
             Array.Sort(setArr);
@@ -35,3 +70,4 @@ using System.Collections.Generic;
         }
     }
 }
+*/
