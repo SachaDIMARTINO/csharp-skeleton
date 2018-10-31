b080ef6db7bada38184eadfc5d36f927be204424
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-31 19:26:32 +0000

    q2

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -1,4 +1,79 @@
-// JUJU
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
+{
+    public class Question2
+    {
+        public static int findMinDist(int[] cashIn, int[] cashOut){
+            int n = cashIn.Length;
+            int m = cashOut.Length;
+            int minDist = -1;
+            // there should at least be a zero in each list
+            if(n==1 && m == 1) return int.MaxValue;
+            else if(n==1) return cashOut[1];
+            else if(m==1) return cashIn[1];
+            else minDist = cashIn[1] < cashOut[1] ? cashIn[1] : cashOut[1];
+            int i = 1;
+            int j = 1;
+            int dist;
+            while(true){
+                dist = Math.Abs(cashIn[i]-cashOut[j]);
+
+                if(dist == 0)
+                    return 0;
+                else if(dist < minDist)
+                    minDist = dist;
+
+                if(cashIn[i] < cashOut[j] && i != n-1)
+                    i += 1;
+                else if(cashIn[i] >= cashOut[j] && j != m-1)
+                    j += 1;
+                else
+                    // we're either at the end of both lists or gonna increase the gap between values
+                    break;
+            }
+            return minDist;
+        }
+        public static int[] sumSetCust2(int[] cashFlow) {
+            int n = cashFlow.Length;
+            int maxSum = 0;
+            for(int i=0; i<n; ++i)
+                maxSum += cashFlow[i];
+
+            bool[] sumSetIdx = new bool[maxSum+1];
+            sumSetIdx[0] = true;
+
+            List<int> sumSet = new List<int>();
+            sumSet.Add(0);
+            for(int i=0; i<n; ++i){
+                List<int> newSumSet = new List<int>();
+                int lenSumSet = sumSet.Count();
+                int cashFlowI = cashFlow[i];
+                for(int j=0; j<lenSumSet; j++){
+                    int newSum = sumSet[j]+cashFlowI;
+                    if(!sumSetIdx[newSum]){
+                        sumSetIdx[newSum] = true;
+                        newSumSet.Add(newSum);
+                    }
+                }
+                for(int j=0; j<newSumSet.Count(); j++){
+                    sumSet.Add(newSumSet[j]);
+                }
+            }
+            sumSet.Sort();
+            return sumSet.ToArray();
+        }
+        public static int Answer(int[] cashflowIn, int[] cashflowOut) {
+            int[] cashIn = sumSetCust2(cashflowIn);
+            int[] cashOut = sumSetCust2(cashflowOut);
+            int minDist = findMinDist(cashIn, cashOut);
+            return minDist;
+        }
+    }
+}
+/*// JUJU
 using System;
 using System.Linq;
 using System.Collections.Generic;
@@ -136,7 +211,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             return minDist;
         }
     }
-}
+}*/
 
 /*// NINI sans TryAdd
 using System.Collections.Generic;
